6a0d7365f132797e149f8b4a08d74f3fa9b526ef
irris <160921248+irrisdev@users.noreply.github.com>
2025-02-07 21:29:18 +0000

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,36 +14,46 @@ else
     exit 1
 fi
 
-# Update and upgrade the system
-apt update && apt upgrade -y
+# Update and Upgrade Packages
+sudo apt update && sudo apt upgrade -y
 
-# Install Fail2Ban for security
-apt install fail2ban -y
+sudo apt install --no-install-recommends -y \
+    unattended-upgrades \
+    fail2ban
 
-# Modify sshd_config to improve security
-sed -i \
-    -e 's/#PermitRootLogin prohibit-password/PermitRootLogin no/g' \
-    -e 's/^PasswordAuthentication.*/PasswordAuthentication no/' \
-    -e 's/^#*UsePAM.*/UsePAM no/' \
+ssh-keygen -t ed25519
+
+sudo sed -i \
+-e 's/#PermitRootLogin prohibit-password/PermitRootLogin no/g' \
+# -e 's/^PasswordAuthentication.*/PasswordAuthentication no/' \
+-e 's/^#*UsePAM.*/UsePAM no/' \
     /etc/ssh/sshd_config
+sudo rm /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
 
-# Remove cloud-init ssh configuration file (if present)
-rm -f /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
+sudo systemctl enable ssh
+sudo systemctl restart ssh
 
-# Enable and restart SSH service
-systemctl enable ssh
-systemctl restart ssh
+# Configure Firewall (UFW)
+sudo ufw enable
+sudo ufw allow ssh
+sudo ufw reload
 
-# Install unattended-upgrades for automatic security updates
-apt install unattended-upgrades -y
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings -y
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Prompt for new username and create the user
-read -p "Enter the username for the new user: " username
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
 
-# Create new user and set password
-adduser "$username"
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-# Add the new user to the sudo group
-usermod -aG sudo "$username"
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
 
-echo "User $username has been created and added to the sudo group."
+sudo apt autoremove -y
